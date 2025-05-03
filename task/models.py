7f1e9a2789f74bbdfcd51326b6dea010ed14b782
Az <arbusinos@up.edu.ph>
2025-05-03 21:53:01 +0800

    [fix][az] alters status field in tasks

--- task/models.py
+++ task/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 class Task(models.Model):
+
+    class Status(models.TextChoices):
+        TODO = "TD", "To Do",
+        IN_PROGRESS = "IP", "In Progress",
+        DONE = "D", "Done",
+        OVERDUE = "OD", "Overdue",
+    
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     assignee = models.ForeignKey("user.CustomUser", on_delete=models.SET_NULL, related_name="task_assignee", null=True, blank=True)
-    status = models.CharField(max_length=50)
+    status = models.CharField(max_length=50, choices=Status, default=Status.TODO)
     due_date = models.DateTimeField(auto_now=False, auto_now_add=False)
     created_on = models.DateTimeField(auto_now=False, auto_now_add=True)
     hive = models.ForeignKey("hive.Hive", on_delete=models.CASCADE, related_name="task_hive")
@@ -13,6 +20,3 @@ class Task(models.Model):
 
     def __str__(self):
         return self.name
-
-    def get_absolute_url(self):
-        return reverse("Task_detail", kwargs={"pk": self.pk})
