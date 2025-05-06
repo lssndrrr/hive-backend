fd2ed1283e6736e8dbedc84ba68e0c3d1b80d248
Az <arbusinos@up.edu.ph>
2025-05-06 16:46:47 +0800

    [fix][az] adds create method to hive viewset

--- hive/viewsets.py
+++ hive/viewsets.py
@@ -4,9 +4,23 @@ from .models import Hive, HiveMembership
 from .serializers import HiveSerializer
 
 
-class HiveViewSet(viewsets.ReadOnlyModelViewSet):
+class HiveViewSet(viewsets.ModelViewSet):
+    queryset = Hive.objects.all()
     serializer_class = HiveSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Hive.objects.filter(hive_membership__user__username=self.request.user.username).distinct()
+
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        hive = serializer.save()
+
+        HiveMembership.objects.create(
+            user=request.user,
+            hive=hive,
+            role=HiveMembership.Role.QUEEN_BEE,
+        )
+
+        return Response({"message": "Hive created successfully.", "data": serializer.data}, status=status.HTTP_201_CREATED)
