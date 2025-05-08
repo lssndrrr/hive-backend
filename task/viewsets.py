b2c60ebd53e2d48e7347748f72afd2fee4d07b8e
Lanieza <lblopez@up.edu.ph>
2025-05-09 03:09:21 +0800

    [feat][lanie] update viewsets to return right format response

--- task/viewsets.py
+++ task/viewsets.py
@@ -1,9 +1,19 @@
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', True)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response({'data': serializer.data}, status=status.HTTP_200_OK) 
