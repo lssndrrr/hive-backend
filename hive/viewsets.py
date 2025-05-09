4c28a3c3db902f27b1b4bd02a5bb18ab5c054a2a
Az <arbusinos@up.edu.ph>
2025-05-10 00:45:48 +0800

    [feat][az] adds invite member method

--- hive/viewsets.py
+++ hive/viewsets.py
@@ -6,7 +6,11 @@ from .serializers import HiveSerializer
 from task.serializers import TaskSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import action
+from django.contrib.auth import get_user_model
+from notifications.models import Invitation, Notification
 
+User = get_user_model()
 
 class HiveViewSet(viewsets.ModelViewSet):
     queryset = Hive.objects.all()
@@ -50,3 +54,33 @@ class HiveViewSet(viewsets.ModelViewSet):
                 "tasks": tasks_data
             }
         }, status=status.HTTP_200_OK)
+    
+    @action(detail=False, methods=['post'])
+    def invite_member(self, request):
+        username = request.data.get('username')
+        hive_id = request.data.get('hive')
+
+        if not username or not hive_id:
+            return Response({'detail': 'Username and hive ID are required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user_to_add = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response({'detail': 'User does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            hive = Hive.objects.get(id=hive_id)
+        except Hive.DoesNotExist:
+            return Response({'detail': 'Hive not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        if HiveMembership.objects.filter(user=user_to_add, hive=hive).exists():
+            return Response({'detail': 'Member is already part of hive.'}, status=status.HTTP_409_CONFLICT)
+
+        if Invitation.objects.filter(recipient=user_to_add, hive=hive).exists():
+            return Response({'detail': 'Invite already sent.'}, status=status.HTTP_200_OK)
+
+        message = f"You have been invited to join the hive '{hive.name}' by {request.user.username}."
+        notification = Notification.objects.create(recipient=user_to_add, message=message, type=Notification.Type.INVITE)
+        Invitation.objects.create(hive=hive, sender=request.user, recipient=user_to_add, notification=notification)
+
+        return Response({'message': 'Invite sent successfully.'}, status=status.HTTP_201_CREATED)
